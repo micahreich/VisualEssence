fdf2ff80bf89c52a8c9714aaa0cbe8f374f0c40a
Micah <micahreich02@gmail.com>
2020-05-29 02:45:56 -0400

    first

---
+++ CNN/CNNDataGen.py
@@ -0,0 +1,58 @@
+import requests
+from PIL import Image
+from random import randint
+from random import sample
+from numpy import asarray
+
+
+class DatasetGenerator:
+    def __init__(self, n_samples):
+        self.n_samples = n_samples
+
+    def get_images(self, save_directory):
+
+        img_ids = [5]
+
+        for i in range(self.n_samples):
+            rand_icon_id = randint(0, 3368879)
+
+            while rand_icon_id in img_ids:
+                rand_icon_id = randint(0, 3368879)
+
+            img_ids.append(rand_icon_id)
+
+            img_url = "https://static.thenounproject.com/png/{}-200.png".format(rand_icon_id)
+
+            with open(save_directory + "/" + str(rand_icon_id) + ".png", 'wb') as f:
+                f.write(requests.get(img_url).content)
+            f.close()
+
+    def create_negative_samples(self, path_to_img_1, path_to_img_2, path_to_img_3):
+
+        # Assuming 200 x 200 images rescaled to 140 x 140 to fit in different locations
+        # within the new 200 x 200 random amalgamation
+        background = Image.open("blank_icon.png")
+        _img_1 = Image.open(path_to_img_1).resize((140, 140), Image.ANTIALIAS)
+        _img_2 = Image.open(path_to_img_2).resize((140, 140), Image.ANTIALIAS)
+        _img_3 = Image.open(path_to_img_3).resize((140, 140), Image.ANTIALIAS)
+
+        # Assuming 200 x 200 background
+        rand_placement_vector = sample(range(0, 60), 2)
+        background.paste(_img_1, (rand_placement_vector[0], rand_placement_vector[1]), _img_1)
+
+        rand_placement_vector = sample(range(0, 60), 2)
+        background.paste(_img_2, (rand_placement_vector[0], rand_placement_vector[1]), _img_2)
+
+        rand_placement_vector = sample(range(0, 60), 2)
+        background.paste(_img_3, (rand_placement_vector[0], rand_placement_vector[1]), _img_3)
+
+        rand_icon_id = randint(0, 3368879)
+        background.save("random_icon_" + str(rand_icon_id) + ".png", "PNG")
+
+    def generate_dataset(self, save_directory):
+        self.get_images(save_directory)
+
+
+DG = DatasetGenerator(5)
+DG.get_images("cnn_data")
+#DG.create_negative_samples("cnn_data/91505.png", "cnn_data/2406058.png", "cnn_data/2730865.png")
---
+++ DescriptorGen.py
@@ -0,0 +1,71 @@
+import csv
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+import re
+
+
+class DescriptorGenerator:
+    def create_stop_words(self, path_to_list):
+        stopwords_list = []
+
+        f = open(path_to_list, "r")
+        for i in f:
+            if len(i) != 0:
+                stopwords_list.append(i.replace("\n", ""))
+
+        return stopwords_list
+
+    def get_descriptions(self, path_to_csv):
+        f = open(path_to_csv)
+        csv_f = csv.reader(f)
+
+        desc_corpus = []
+
+        i = 0
+        for row in csv_f:
+            if len(row) != 0 and len(row[27]) != 0 and len(row[28]) != 0 and len(row[29]) != 0:
+                desc_corpus.append(row[27].strip())
+                desc_corpus.append(row[28].strip())
+                desc_corpus.append(row[29].strip())
+
+                i += 1
+
+        for j in range(len(desc_corpus)):
+            desc_corpus[j] = desc_corpus[j].lower()
+            regex = re.compile('[1234567890,./?]')
+            desc_corpus[j] = regex.sub('', str(desc_corpus[j]))
+
+        del desc_corpus[0:3]
+
+        return desc_corpus
+
+    def tf_idf(self, train_corpus, n_terms=1):
+        tfidf_vectorizer = TfidfVectorizer(use_idf=True, stop_words=self.create_stop_words('model_data/stopwords.txt'))
+        tfidf_vectorizer.fit_transform(train_corpus)
+
+        tfidf_descriptors = []
+
+        new_descriptors = []
+
+        for i in range(0, len(train_corpus)):
+
+            tfidf_desc = tfidf_vectorizer.transform([train_corpus[i]])
+            feature_array = np.array(tfidf_vectorizer.get_feature_names())
+
+            tfidf_sorting = np.argsort(tfidf_desc.toarray()).flatten()[::-1]
+
+            top_n = feature_array[tfidf_sorting][:n_terms]
+
+            new_descriptors.append(top_n[0])
+
+            if (i+1) % 3 == 0:
+                tfidf_descriptors.append(new_descriptors)
+                new_descriptors = []
+
+        return tfidf_descriptors
+
+
+Descriptions = DescriptorGenerator()
+data = Descriptions.get_descriptions('model_data/450DESC.csv')
+print(np.array(Descriptions.tf_idf(data))[0])
+
