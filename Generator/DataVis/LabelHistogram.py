a34c144574ac0378ee841d668e7e1f4c07136346
Micah <micahreich02@gmail.com>
2020-06-19 16:18:27 -0400

    lhist

--- Generator/DataVis/LabelHistogram.py
+++ Generator/DataVis/LabelHistogram.py
@@ -2,50 +2,45 @@ from PIL import Image
 import numpy as np
 import os
 import numpy as np
+import math
 import matplotlib.mlab as mlab
+import pickle
 import matplotlib.pyplot as plt
 import random
 
 
-class PixelHistogram:
+class LabelHistogram:
     def __init__(self, data_directory):
         self.data_directory = data_directory
 
-    def count_black_px(self, path_to_image, pos_neg_sample):
-        black_px_count = 0
-        image_arr = np.asarray(Image.open(path_to_image))
+    def get_labels(self):
+        pickled_labels = pickle.load(open((self.data_directory + '/all/pkl_labels_all.pkl'), 'rb'))
 
-        if pos_neg_sample == "POSITIVE":
-            image_arr = (255-image_arr)[:, :, 3]
-        elif pos_neg_sample == "NEGATIVE":
-            image_arr = image_arr[:, :, 2]
+        x = np.asarray(pickled_labels)[:,:,0]
+        y = np.asarray(pickled_labels)[:,:,1]
 
-        return np.count_nonzero(image_arr < 255)
-
-    def get_pixel_data(self):
-        positive_sample_px = []
-        negative_sample_px = []
-
-        files = os.listdir(self.data_directory)
-
-        for i in files:
-            if i[0].upper() == "I":
-                positive_sample_px.append(self.count_black_px(self.data_directory + "/" + i, "POSITIVE"))
-            elif i[0].upper() == "R":
-                negative_sample_px.append(self.count_black_px(self.data_directory + "/" + i, "NEGATIVE"))
-
-        return positive_sample_px, negative_sample_px
+        return np.asarray(pickled_labels).flatten().tolist(), \
+               np.asarray(x).flatten().tolist(), \
+               np.asarray(y).flatten().tolist()
 
     def plot_histogram(self):
-        pixel_counts = self.get_pixel_data()
+        all_coords, x, y = self.get_labels()
 
-        plt.hist(pixel_counts[0], 10, alpha=0.5, label='Positive Samples')
-        plt.hist(pixel_counts[1], 10, alpha=0.5, label='Negative Samples')
+        bin_size_1 = 1 + 3.322*math.log(len(all_coords), 10)
+        bin_size_2 = 1 + 3.322*math.log(len(x), 10)
+
+        plt.hist(all_coords, int(bin_size_1), alpha=0.5, label='All Coordinates')
         plt.legend(loc='upper right')
-        plt.savefig("PixelHistogram.png")
+        plt.savefig("AllCoords.png")
+        plt.close()
+
+        plt.hist(x, int(bin_size_2), alpha=0.5, label='X')
+        plt.hist(y, int(bin_size_2), alpha=0.5, label='Y')
+        plt.legend(loc='upper right')
+        plt.savefig("XYCoords.png")
 
 
 if __name__ == "__main__":
     # "/nethome/mreich8/VisualEssence/data/cnn_data_backup/cnn_data"
-    DataVisualizer = PixelHistogram("/Users/micahreich/Documents/VisualEssence/data/cnn_data")
-    DataVisualizer.plot_histogram()
+    DV = LabelHistogram("/Users/micahreich/Documents/VisualEssence/data")
+    DV.plot_histogram()
